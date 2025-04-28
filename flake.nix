bbcebd3524bb5d273c6c41a727c60e72c04aae92
gjhenrique <me@gjhenrique.com>
2025-04-28 09:55:03 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A flake with curl and jq";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = {
+          curl = pkgs.curl;
+          jq = pkgs.jq;
+        };
+      }
+    );
+}
